e5deaed79645d3fde94530ab52113672b54ce1b1
Vinit Kumar <vinitkumar5893@gmail.com>
2023-11-27 19:15:15 +0530

    create spiral date set and update the 2nd code according the video

--- 2. 2nd code.py
+++ 2. 2nd code.py
@@ -184,8 +184,49 @@ print(output)
 # -----------------------------------------------------------------------------------------------------------------#
 
 # ----------------------------------- Usage of Activation Function (ReLU) -----------------------------------------#
+'''
 import numpy as np
 np.random.seed(0)
+
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
+
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons) # 0.10 is used to scale the values to a useful range
+        # np.random.randn() is used to create an array of random numbers 
+        # n_inputs is the number of inputs and n_neurons is the number of neurons
+        # so the shape of the array will be (n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons)) # np.zeros() is used to create an array of zeros # shape = (1, n_neurons)
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+# print(0.10*np.random.randn(4, 3))
+
+layer1 = Layer_Dense(4, 5) # 4 inputs, 5 neurons
+layer2 = Layer_Dense(5, 2) # 5 inputs, 2 neurons
+
+layer1.forward(X)
+#print(layer1.output)
+layer2.forward(layer1.output)
+print(layer2.output)
+'''
+# -----------------------------------------------------------------------------------------------------------------#
+
+# ----------------------------------- Usage of Activation Function(RELU) nnfs--------------------------------------#
+import numpy as np
+import nnfs
+
+nnfs.init()
+
 
 X = [[1, 2, 3, 2.5],
      [2.0, 5.0, -1.0, 2.0],
