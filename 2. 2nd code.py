03970608d52f4a2e40097c6d0da04eff42940966
Vinit Kumar <vinitkumar5893@gmail.com>
2023-12-25 15:33:33 +0530

    add comment

--- 2. 2nd code.py
+++ 2. 2nd code.py
@@ -313,7 +313,7 @@ activation1.forward(layer1.output)
 layer2.forward(activation1.output)
 activation2.forward(layer2.output)
 
-print(activation2.output[:5])
+print(activation2.output[:5]) # print first 5 outputs
 
 # print(np.sum(activation2.output, axis=1, keepdims=True))
 
